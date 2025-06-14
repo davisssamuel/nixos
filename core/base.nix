642884ecdf8fae5ee33ec29cf1416527912f3728
davisssamuel <davissamuelg1@gmail.com>
2025-06-14 10:26:02 -0400

    Added neovim to user pkgs under base.nix

--- core/base.nix
+++ core/base.nix
@@ -49,6 +49,9 @@
       "networkmanager"
     ];
     shell = pkgs.zsh;
+    packages = with pkgs; [
+      neovim
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWklVXOkuctgqNhRa7BnysWiB9ZtQmrAdCrxvrFnjb3 sam@macbook.local"
     ];
