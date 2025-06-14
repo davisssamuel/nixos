9fea04a9d0b92c5ad52ce9dc363c7defec5e4c6f
davisssamuel <davissamuelg1@gmail.com>
2025-06-13 22:44:34 -0400

    Added authorized key to user for ssh

--- core/base.nix
+++ core/base.nix
@@ -38,6 +38,9 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWklVXOkuctgqNhRa7BnysWiB9ZtQmrAdCrxvrFnjb3 sam@macbook.local"
+    ];
   };
 
   programs.zsh = {
