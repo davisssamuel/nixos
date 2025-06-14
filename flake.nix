bb4b395d4b2d203512aecbfd720d087ea0232444
davisssamuel <davissamuelg1@gmail.com>
2025-06-13 22:14:39 -0400

    Fixed system attribute by sharing it

--- flake.nix
+++ flake.nix
@@ -5,14 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
   };
 
-  outputs = {self, nixpkgs, ...}: {
-	
+  outputs = {self, nixpkgs, ...}: 
+  let
     system = "x86_64-linux";
-
+  in {
 	formatter = (system: nixpkgs.legacyPackages.${system}.nixfmt);
 
     nixosConfigurations = {
 	  case = nixpkgs.lib.nixosSystem {
+        inherit system;
 	    specialArgs = {inherit inputs outputs;};
 	    modules = [./machines/case/configuration.nix];
 	  };
