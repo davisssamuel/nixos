7a741a5e49f341852d868a3740485f62a8e9633d
davisssamuel <davissamuelg1@gmail.com>
2025-06-19 12:29:41 -0400

    fix: added missing character to caseTunnelId

--- vars.nix
+++ vars.nix
@@ -1,5 +1,5 @@
 {
   username = "sam";
   macbookPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWklVXOkuctgqNhRa7BnysWiB9ZtQmrAdCrxvrFnjb3 sam@macbook.local";
-  caseTunnelId = "bb45604a-1292-447a-a349-d619f43b798";
+  caseTunnelId = "bb45604a-1292-447a-a349-d619f43b798f";
 }
