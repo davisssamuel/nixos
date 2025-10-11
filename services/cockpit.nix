cbdef9a7ca640fc77a603a27007add2984e3bf4f
davisssamuel <davissamuelg1@gmail.com>
2025-10-11 12:05:45 -0400

    fix: added pkgs var to cockpit config

---
+++ services/cockpit.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+{
+  environment.systemPackages = with pkgs; [
+    cockpit
+  ];
+
+  services.cockpit = {
+    enable = true;
+    port = 9090;
+    openFirewall = true;
+    settings = {
+      WebService = {
+        AllowUnencrypted = true;
+      };
+    };
+  };
+}
