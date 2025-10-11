e946764b9536e0c548b3781965d42bc5423eeaae
davisssamuel <davissamuelg1@gmail.com>
2025-10-11 14:49:10 -0400

    fix: add allowed-origins to cockpit config

--- services/cockpit.nix
+++ services/cockpit.nix
@@ -17,5 +17,10 @@
         AllowUnencrypted = true;
       };
     };
+    allowed-origins = [
+      "http://100.99.70.29:9090"
+      "http://192.168.200.235:9090"
+      "https://case.local"
+    ];
   };
 }
