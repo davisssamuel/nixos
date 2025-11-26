de2bc841095c154bfc49ad5c797f9029be03fef4
davisssamuel <davissamuelg1@gmail.com>
2025-11-26 17:04:38 -0500

    feat: set world type in minecraft-server config

--- services/minecraft-server.nix
+++ services/minecraft-server.nix
@@ -9,14 +9,19 @@
     eula = true;
     openFirewall = true; # Port 25565
     declarative = true;
+
     package = pkgs.minecraftServers.vanilla-1-8;
+
     serverProperties = {
-      gamemode = 0;
-      difficulty = 3;
+      gamemode = "survival";
+      difficulty = "hard";
       allow-cheats = false;
+      level-type = "minecraft:large_biomes";
       max-players = 10;
+      player-idle-timeout = 120;
       motd = "Classic Minecraft";
     };
-    jvmOpts = "-Xms4G -Xmx8G";
+
+    jvmOpts = "-Xms4G -Xmx8G -XX:+UseG1GC";
   };
 }
