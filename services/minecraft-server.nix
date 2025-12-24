7b60abf2b053494255ac0152555024efaa4516e1
davisssamuel <davissamuelg1@gmail.com>
2025-12-24 15:43:39 -0500

    fix: lag issue on minecraft server

--- services/minecraft-server.nix
+++ services/minecraft-server.nix
@@ -13,11 +13,12 @@
       difficulty = "normal";
       allow-cheats = false;
       level-type = "minecraft:large_biomes";
+      server-ip = "0.0.0.0";
       max-players = 10;
       player-idle-timeout = 120;
       motd = "Classic Minecraft";
     };
 
-    jvmOpts = "-Xms4G -Xmx8G";
+    jvmOpts = "-Xmx4G -XX:+UseG1GC -XX:MaxGCPauseMillis=50";
   };
 }
