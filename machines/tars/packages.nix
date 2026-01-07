f0175ba1ce727330c75be50f55f1772b1f1ae3aa
Samuel Davis <davissamuelg1@gmail.com>
2026-01-06 21:42:24 -0500

    feat: added noto-fonts-color-emoji to tars

--- machines/tars/packages.nix
+++ machines/tars/packages.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     alacritty
     chromium
     commit-mono
     corefonts
     noto-fonts
+    noto-fonts-color-emoji
     noto-fonts-lgc-plus
   ];
 }
