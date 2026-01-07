d54ac48163d06cc8871eacf44654aca7793266c6
davisssamuel <davissamuelg1@gmail.com>
2026-01-06 21:03:26 -0500

    fix: updated the nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
   };
 
   outputs =
