4484acb8c8d5dcb695cb17debfc860df6f3cfa82
davisssamuel <davissamuelg1@gmail.com>
2026-01-06 22:21:13 -0500

    feat: added casePublicKey

--- vars.nix
+++ vars.nix
@@ -1,6 +1,6 @@
 {
   username = "sam";
-  casePublicKey = "";
+  casePublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKTqFNnK06xjVR846L+yJvsCOun/hz6F70W8cSiizOZk sam@case";
   caseTunnelId = "bb45604a-1292-447a-a349-d619f43b798f";
   macbookPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWklVXOkuctgqNhRa7BnysWiB9ZtQmrAdCrxvrFnjb3 sam@macbook.local";
   tarsPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKa8l3Vfze1Bxr3rldUgu29Ud59c+vHEFGU+kX7uabxE sam@tars";
