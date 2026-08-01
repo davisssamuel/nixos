ea45d3d314e690ece21d1778cce4c769483bbc17
davisssamuel <davissamuelg1@gmail.com>
2026-08-01 09:54:43 -0400

    feat: added headscale service

---
+++ services/headscale.nix
@@ -0,0 +1,19 @@
+{ ... }:
+{
+  services.headscale = {
+    enable = true;
+
+    settings = {
+      server_url = "https://headscale.kyncayd.com";
+
+      dns = {
+        magic_dns = true;
+        base_domain = "tailnet.kyncayd.com";
+      };
+
+      prefixes = [
+        "100.64.0.0/10"
+      ];
+    };
+  };
+}
